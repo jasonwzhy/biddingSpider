a5076bb4267e6460d11eae4cb81145d6fc560038
jasonwzhy <jasonwzhy@gmail.com>
2015-03-18 00:37:37 +0800

    up

--- bidding/spiders/chinabidding_spider.py
+++ bidding/spiders/chinabidding_spider.py
@@ -3,7 +3,14 @@ class ChinaBiddingSpider(scrapy.Spider):
 	name = "chinabidding"
 	allow_domains = ["chinabidding.com"]
 	start_urls=[
+		"https://cas.ebnew.com/cas/login",
 		"http://www.chinabidding.com/search/proj.htm"
 	]
+	def start_requests(self):
+		return [scrapy.FormRequest("https://cas.ebnew.com/cas/login",
+									formdata = {'username':'p131','password':'zp0611zp'},
+									callback = self.onlogin)]
 	def parse(self,response):
+		pass
+	def onlogin(self,response):
 		pass
