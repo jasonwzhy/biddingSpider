8e0fda10d48d97c5d80919d98fd9491d13e15f7b
jasonwzhy <jasonwzhy@gmail.com>
2015-03-27 01:22:15 +0800

    up

--- bidding/settings.py
+++ bidding/settings.py
@@ -12,6 +12,22 @@ BOT_NAME = 'bidding'
 
 SPIDER_MODULES = ['bidding.spiders']
 NEWSPIDER_MODULE = 'bidding.spiders'
+DOWNLOADER_MIDDLEWARES_BASE = {
+    'scrapy.contrib.downloadermiddleware.robotstxt.RobotsTxtMiddleware': 100,
+    'scrapy.contrib.downloadermiddleware.httpauth.HttpAuthMiddleware': 300,
+    'scrapy.contrib.downloadermiddleware.downloadtimeout.DownloadTimeoutMiddleware': 350,
+    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': 400,
+    'scrapy.contrib.downloadermiddleware.retry.RetryMiddleware': 500,
+    'scrapy.contrib.downloadermiddleware.defaultheaders.DefaultHeadersMiddleware': 550,
+    'scrapy.contrib.downloadermiddleware.redirect.MetaRefreshMiddleware': 580,
+    'scrapy.contrib.downloadermiddleware.httpcompression.HttpCompressionMiddleware': 590,
+    'scrapy.contrib.downloadermiddleware.redirect.RedirectMiddleware': 600,
+    'scrapy.contrib.downloadermiddleware.cookies.CookiesMiddleware': 700,
+    'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 750,
+    'scrapy.contrib.downloadermiddleware.chunked.ChunkedTransferMiddleware': 830,
+    'scrapy.contrib.downloadermiddleware.stats.DownloaderStats': 850,
+    'scrapy.contrib.downloadermiddleware.httpcache.HttpCacheMiddleware': 900,
+}
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'bidding (+http://www.yourdomain.com)'
--- bidding/spiders/chinabidding_spider.py
+++ bidding/spiders/chinabidding_spider.py
@@ -2,15 +2,21 @@ import scrapy
 class ChinaBiddingSpider(scrapy.Spider):
 	name = "chinabidding"
 	allow_domains = ["chinabidding.com"]
-	start_urls=[
-		"https://cas.ebnew.com/cas/login",
-		"http://www.chinabidding.com/search/proj.htm"
-	]
+	# start_urls=[
+	# 	"https://cas.ebnew.com/cas/login",
+	# 	"http://www.chinabidding.com/search/proj.htm",
+	#	"http://www.chinabidding.com/bid/index/loginIndex.htm"
+	# ]
 	def start_requests(self):
 		return [scrapy.FormRequest("https://cas.ebnew.com/cas/login",
-									formdata = {'username':'p131','password':'zp0611zp'},
+									formdata = {'username':'p131','password':'zp0611zp','authorize':'true',
+									'loginUrl':'http://www.chinabidding.com/bid/index/loginIndex.htm',
+									'service':'http://www.chinabidding.com/bid/login/login.htm',
+									'realService':'http://211.151.182.227:8091'},
 									callback = self.onlogin)]
 	def parse(self,response):
 		pass
 	def onlogin(self,response):
-		pass
+		filename = response.url.split("/")[-2]
+		with open(filename, 'wb') as f:
+			f.write(response.body)
